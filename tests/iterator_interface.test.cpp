8518c2c1306af67e3bf3a88b989b1ccd410cac86
krzysiek <krzysztof.muhleisen@gmail.com>
2025-06-22 20:21:57 +0200

    iterator_interface test

--- tests/iterator_interface.test.cpp
+++ tests/iterator_interface.test.cpp
@@ -6,11 +6,11 @@ struct iter_impl
 {
     int m_value;
 
-    iter_impl(int value = 0) : m_value(value)
+    explicit iter_impl(int value = 0) : m_value(value)
     {
     }
 
-    int deref() const
+    auto deref() const -> int
     {
         return m_value;
     }
@@ -20,7 +20,7 @@ struct iter_impl
         m_value += 10 * offset;
     }
 
-    std::ptrdiff_t distance_to(const iter_impl& other) const
+    auto distance_to(const iter_impl& other) const -> std::ptrdiff_t
     {
         return (other.m_value - m_value) / 10;
     }
@@ -36,13 +36,28 @@ TEST_CASE("iterator_interface", "[iterator]")
 TEST_CASE("iterator_interface - advance", "[iterator]")
 {
     const auto iter = test_iterator{ 10 };
-    REQUIRE(*iter == 10);
-    REQUIRE(iter[1] == 20);
+    REQUIRE_THAT(*iter, matchers::equal_to(10));
+    REQUIRE_THAT(iter[1], matchers::equal_to(20));
 }
 
 TEST_CASE("iterator_interface - distance", "[iterator]")
 {
     const auto lhs = test_iterator{ 10 };
     const auto rhs = test_iterator{ 50 };
-    REQUIRE(std::distance(lhs, rhs) == 4);
+    REQUIRE_THAT(std::distance(lhs, rhs), matchers::equal_to(4));
+}
+
+TEST_CASE("iterator_interface - values", "[iterator]")
+{
+    REQUIRE_THAT(
+        std::accumulate(
+            test_iterator{ 10 },
+            test_iterator{ 50 },
+            std::vector<int>{},
+            [](std::vector<int> acc, int v) -> std::vector<int>
+            {
+                acc.push_back(v);
+                return acc;
+            }),
+        matchers::elements_are(10, 20, 30, 40));
 }
