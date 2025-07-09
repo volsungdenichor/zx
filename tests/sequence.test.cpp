cc467dfbcc36be1b978ac86f98147164490b46b4
krzysiek <krzysztof.muhleisen@gmail.com>
2025-07-09 22:48:36 +0200

    To map conversion

--- tests/sequence.test.cpp
+++ tests/sequence.test.cpp
@@ -8,7 +8,7 @@ constexpr auto associate(Func func)
     return [=](auto&& item)
     {
         auto&& res = std::invoke(func, item);
-        return std::tuple{ std::forward<decltype(item)>(item), res };
+        return std::pair{ std::forward<decltype(item)>(item), res };
     };
 }
 
@@ -30,7 +30,7 @@ TEST_CASE("sequence - transform", "[sequence][transform]")
     REQUIRE_THAT(
         zx::seq::range(5).transform(associate([](int x) { return x * x; })),
         matchers::elements_are(
-            std::tuple{ 0, 0 }, std::tuple{ 1, 1 }, std::tuple{ 2, 4 }, std::tuple{ 3, 9 }, std::tuple{ 4, 16 }));
+            std::pair{ 0, 0 }, std::pair{ 1, 1 }, std::pair{ 2, 4 }, std::pair{ 3, 9 }, std::pair{ 4, 16 }));
 }
 
 TEST_CASE("sequence - filter", "[sequence][filter]")
@@ -43,4 +43,16 @@ TEST_CASE("sequence - slice", "[sequence][slicing]")
     REQUIRE_THAT(zx::seq::range(10).slice({ 1, 4 }), matchers::elements_are(1, 2, 3));
     REQUIRE_THAT(zx::seq::range(10).slice({ {}, 4 }), matchers::elements_are(0, 1, 2, 3));
     REQUIRE_THAT(zx::seq::range(10).slice({ 4, {} }), matchers::elements_are(4, 5, 6, 7, 8, 9));
+}
+
+TEST_CASE("sequence - associate", "[sequence][transform]")
+{
+    const std::map<int, std::string> map
+        = zx::seq::range(5).transform(associate([](int x) { return zx::str('|', x * (x + 2), '|'); }));
+    REQUIRE_THAT(map, matchers::size_is(5));
+    REQUIRE_THAT(map.at(0), matchers::equal_to("|0|"));
+    REQUIRE_THAT(map.at(1), matchers::equal_to("|3|"));
+    REQUIRE_THAT(map.at(2), matchers::equal_to("|8|"));
+    REQUIRE_THAT(map.at(3), matchers::equal_to("|15|"));
+    REQUIRE_THAT(map.at(4), matchers::equal_to("|24|"));
 }
