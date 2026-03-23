25b26f0aaa056ebd9758d41e7d0b0dbaaae34fd0
krzysiek <krzysztof.muhleisen@gmail.com>
2026-03-23 17:26:52 +0100

    cross-platform test expectations in type_name

--- modules/core/tests/type_name.test.cpp
+++ modules/core/tests/type_name.test.cpp
@@ -5,6 +5,6 @@
 TEST(debug, type_name)
 {
     EXPECT_THAT(zx::type_name<int>(), testing::Eq("int"));
-    EXPECT_THAT(zx::type_name<std::string>(), testing::Eq("std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >"));
-    EXPECT_THAT(zx::type_name<std::vector<int>>(), testing::Eq("std::vector<int, std::allocator<int> >"));
+    EXPECT_THAT(zx::type_name<std::string>(), testing::HasSubstr("basic_string"));
+    EXPECT_THAT(zx::type_name<std::vector<int>>(), testing::HasSubstr("vector"));
 }
