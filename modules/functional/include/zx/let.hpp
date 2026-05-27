60b071a2c8fe2acd1df94fbd25e21da9db6e4683
krzysiek <krzysztof.muhleisen@gmail.com>
2026-05-27 21:38:40 +0200

    let: add support for any number of arguments

--- modules/functional/include/zx/let.hpp
+++ modules/functional/include/zx/let.hpp
@@ -17,14 +17,17 @@ template <class... Args>
 struct let_result<std::tuple<Args...>>
 {
 private:
+    static constexpr std::size_t args_count = sizeof...(Args);
+    using tuple_type = std::tuple<Args...>;
+    using func_type = std::tuple_element_t<args_count - 1, tuple_type>;
+    using index_sequence = std::make_index_sequence<args_count - 1>;
+
     template <std::size_t... I>
     static auto make_type(std::index_sequence<I...>)
-        -> std::invoke_result_t<
-            std::tuple_element_t<sizeof...(Args) - 1, std::tuple<Args...>>,
-            std::tuple_element_t<I, std::tuple<Args...>>...>;
+        -> std::invoke_result_t<func_type, std::tuple_element_t<I, tuple_type>...>;
 
 public:
-    using type = decltype(make_type(std::make_index_sequence<sizeof...(Args) - 1>{}));
+    using type = decltype(make_type(index_sequence{}));
 };
 
 template <class... Args>
@@ -35,18 +38,16 @@ struct let_fn
     template <class... Args>
     constexpr auto operator()(Args&&... args) const -> let_result_t<Args&&...>
     {
-        static_assert(sizeof...(Args) >= 1, "zx::let requires a callable");
-        return impl(std::make_index_sequence<sizeof...(Args) - 1>{}, std::forward<Args>(args)...);
+        constexpr std::size_t args_count = sizeof...(Args);
+        return impl<args_count - 1>(
+            std::make_index_sequence<args_count - 1>{}, std::forward_as_tuple(std::forward<Args>(args)...));
     }
 
 private:
-    template <std::size_t... I, class... Args>
-    static constexpr decltype(auto) impl(std::index_sequence<I...>, Args&&... args)
+    template <std::size_t Last, std::size_t... I, class Tuple>
+    static constexpr decltype(auto) impl(std::index_sequence<I...>, Tuple&& tuple)
     {
-        auto tuple = std::forward_as_tuple(std::forward<Args>(args)...);
-        return std::invoke(
-            std::get<sizeof...(Args) - 1>(std::move(tuple)),
-            std::get<I>(std::move(tuple))...);
+        return std::invoke(std::get<Last>(std::forward<Tuple>(tuple)), std::get<I>(std::forward<Tuple>(tuple))...);
     }
 };
 
